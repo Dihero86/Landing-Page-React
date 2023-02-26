e4a1e8cdbb02eba73a9943984972eefa9062d44d
Dihero86 <diegohrodriguez86@gmail.com>
2023-02-26 16:59:12 +0000

    feach: First Version

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer=()=>{
+ return(
+        <div className="text-center py-3 bg-dark text-white mb-0">
+        © 2023 Copyright
+        </div>
+ )}
+
+ export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,23 @@
 import React from "react";
+import NavBar from "./navbar.jsx";
+import Jumbotrom from "./jumbotrom.jsx";
+import MyCard from "./cards.jsx";
+import Footer from "./footer.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+const Home=()=> {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<NavBar />
+			<div className="container">
+				<Jumbotrom />
+				<div className="row justify-content-around">
+					<MyCard foto="https://images.unsplash.com/photo-1677312887323-5c7a378a2630?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"/>
+					<MyCard foto="https://images.unsplash.com/photo-1672243775941-10d763d9adef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
+					<MyCard foto="https://images.unsplash.com/photo-1675180547490-b93306b2ca3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"/>
+					<MyCard foto="https://images.unsplash.com/photo-1676625195945-0b12142742f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
+				</div>
+			</div>
+			<Footer />
 		</div>
 	);
 };
